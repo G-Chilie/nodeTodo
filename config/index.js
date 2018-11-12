4ef1e37b3bdfd3f82150dab0850d356d512c5a22
G-Chilie <okonkwolinda7@gmail.com>
2018-11-12 11:16:45 -0800

    requiring config

---
+++ config/index.js
@@ -0,0 +1,9 @@
+var configValues = require('./config');
+
+module.exports = {
+
+    getDbConnectionString: finction() {
+        return 'mongodb://' +configValues.uname + ':' + configValues.pwd + '@ds033669.mlab.com:33669/chinwe';
+    }
+
+}
